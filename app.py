dc669b384a917114c9b3bee258ff98ffb33e64de
Sandy <sangzicao@gmail.com>
2019-02-20 16:44:29 -0800

    added delete / edit functionality

--- app.py
+++ app.py
@@ -36,13 +36,13 @@ def show_create_user_form():
     return render_template("create_user_form.html")
 
 @app.route('/users', methods=["POST"])
-def creat_new_users():
+def create_new_users():
     """process the add form and adding new users to db
     redirect back to /users"""
 
     first_name = request.form['first_name']
     last_name = request.form['last_name']
-    image_url = request.form['image_url']
+    image_url = request.form['image_url'] or None
 
     user = User(first_name = first_name, last_name = last_name, image_url = image_url)
     db.session.add(user)
@@ -53,4 +53,47 @@ def creat_new_users():
 @app.route('/users/<user_id>')
 def user_profile(user_id):
 
-    return "hi"
+    user = User.query.get(user_id)
+
+    first_name = user.first_name
+    last_name = user.last_name
+    image_url = user.image_url
+
+    return render_template('user_detail.html', first_name=first_name, last_name=last_name, image_url=image_url, user_id=user_id)
+
+@app.route('/users/<user_id>/edit')
+def show_edit_user_profile(user_id):
+    """Show form for editing user data"""
+
+    user = User.query.get(user_id)
+
+    first_name = user.first_name
+    last_name = user.last_name
+    image_url = user.image_url
+
+    return render_template('edit_user_form.html', first_name=first_name, last_name=last_name, image_url=image_url, user_id=user_id)
+
+@app.route('/users/<user_id>/edit', methods=["POST"])
+def edit_user_profile(user_id):
+    """Edit the user profile based on the User ID"""
+
+    user = User.query.get(user_id)
+
+    user.first_name = request.form.get('first_name')
+    user.last_name = request.form.get('last_name')
+    user.image_url = request.form.get('image_url') or None
+
+    db.session.add(user)
+    db.session.commit()
+
+    return redirect('/users')
+
+@app.route('/users/<user_id>/delete', methods=["POST"])
+def delete_user_profile(user_id):
+    """Deletes the user profile"""
+
+    user = User.query.get(user_id)
+    db.session.delete(user)
+    db.session.commit()
+
+    return redirect('/users')
